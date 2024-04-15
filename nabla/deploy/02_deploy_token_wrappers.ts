4e9ac41520fabbf3f5a8a1e74d39b70ed307f162
Torsten Stüber <15174476+TorstenStueber@users.noreply.github.com>
2024-04-15 06:22:53 -0300

    Add deployment for five swap pools

--- nabla/deploy/02_deploy_token_wrappers.ts
+++ nabla/deploy/02_deploy_token_wrappers.ts
@@ -4,22 +4,34 @@ import { isTestnet } from "../_lib";
 async function DeployMockTokens({ getNamedAccounts, deployments }: WasmDeployEnvironment) {
   const { deployer } = await getNamedAccounts();
 
-  await deployments.deploy("mUSD", {
+  await deployments.deploy("nativeErc20Wrapper", {
     from: deployer,
     contract: "ERC20Wrapper",
     args: ["Native", "NAT", 12, [0], [0], [], []],
     log: true,
   });
-  await deployments.deploy("mEUR", {
+  await deployments.deploy("xcm0Erc20Wrapper", {
     from: deployer,
     contract: "ERC20Wrapper",
-    args: ["XCM0", "X0", 12, [1], [0], [], []],
+    args: ["XCM0", "X0", 7, [1], [0], [], []],
     log: true,
   });
-  await deployments.deploy("mETH", {
+  await deployments.deploy("xcm1Erc20Wrapper", {
     from: deployer,
     contract: "ERC20Wrapper",
-    args: ["XCM1", "X1", 12, [1], [1], [], []],
+    args: ["XCM1", "X1", 18, [1], [1], [], []],
+    log: true,
+  });
+  await deployments.deploy("xcm2Erc20Wrapper", {
+    from: deployer,
+    contract: "ERC20Wrapper",
+    args: ["XCM2", "X2", 15, [1], [2], [], []],
+    log: true,
+  });
+  await deployments.deploy("xcm3Erc20Wrapper", {
+    from: deployer,
+    contract: "ERC20Wrapper",
+    args: ["XCM3", "X3", 10, [1], [3], [], []],
     log: true,
   });
 }
