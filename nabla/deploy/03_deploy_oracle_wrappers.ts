ee6d53f6b8272ed601c229e36b7a9b812babbc44
Torsten Stüber <15174476+TorstenStueber@users.noreply.github.com>
2024-04-19 14:27:55 -0300

    Add missing calls to Nabla deployment

--- nabla/deploy/03_deploy_oracle_wrappers.ts
+++ nabla/deploy/03_deploy_oracle_wrappers.ts
@@ -4,10 +4,12 @@ import { isTestnet, registerAsset } from "../_lib";
 async function DeployMockOracles({ getNamedAccounts, deployments }: WasmDeployEnvironment) {
   const { deployer } = await getNamedAccounts();
 
-  const [native, xcm0, xcm1] = await Promise.all([
+  const [native, xcm0, xcm1, xcm2, xcm3] = await Promise.all([
     deployments.get("nativeErc20Wrapper"),
     deployments.get("xcm0Erc20Wrapper"),
     deployments.get("xcm1Erc20Wrapper"),
+    deployments.get("xcm2Erc20Wrapper"),
+    deployments.get("xcm3Erc20Wrapper"),
   ]);
 
   const priceFeedWrapper = await deployments.deploy("priceFeedWrapper", {
@@ -18,6 +20,8 @@ async function DeployMockOracles({ getNamedAccounts, deployments }: WasmDeployEn
         { asset: native.address, blockchain: "Native", symbol: "NAT" },
         { asset: xcm0.address, blockchain: "XCM0", symbol: "X0" },
         { asset: xcm1.address, blockchain: "XCM1", symbol: "X1" },
+        { asset: xcm2.address, blockchain: "Polkadot", symbol: "DOT" },
+        { asset: xcm3.address, blockchain: "Stellar", symbol: "XLM" },
       ],
     ],
     log: true,
@@ -26,6 +30,8 @@ async function DeployMockOracles({ getNamedAccounts, deployments }: WasmDeployEn
   await registerAsset(deployments, { from: deployer, log: true }, native.address, priceFeedWrapper.address);
   await registerAsset(deployments, { from: deployer, log: true }, xcm0.address, priceFeedWrapper.address);
   await registerAsset(deployments, { from: deployer, log: true }, xcm1.address, priceFeedWrapper.address);
+  await registerAsset(deployments, { from: deployer, log: true }, xcm2.address, priceFeedWrapper.address);
+  await registerAsset(deployments, { from: deployer, log: true }, xcm3.address, priceFeedWrapper.address);
 }
 
 DeployMockOracles.tags = ["oracles"];
